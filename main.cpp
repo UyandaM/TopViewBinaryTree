6ce9c017160ee596147563cf737ee16c369d013c
UyandaM <uyandamphunga@gmail.com>
2020-04-12 23:00:24 +0200

    topview algorithm

--- main.cpp
+++ main.cpp
@@ -1,3 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/*
+ * File:   main.cpp
+ * Author: uyanda
+ *
+ * Created on 11 April 2020, 12:21 PM
+ */
+
 #include <iostream>
 #include <vector>
 
@@ -13,14 +26,21 @@ class node
     node* right;
 };
 
-int maxDepth(node* node);
-node* newNode(int data);
-void treeDepth(node* node, int depth, vector<int>& depths, vector<int>& data);
-void treeWidth(node* node, int width, vector<int>& widths);
-void widthSort(vector<int>& widths, vector<int>& depths, vector<int>& data);
+class keyvalue
+{
+    public:
+    int data;
+    int width;
+    int depth;
+};
 
+keyvalue keypair;
+node* newNode(int data);
+
+void topView(node * root);
+void loadTopView(node* root, int width, int depth, vector<keyvalue>& treelist);
 // Driver code
-int main()
+int main(int argc, char** argv)
 {
     node *root = newNode(1);
 
@@ -29,48 +49,11 @@ int main()
     root->left->left = newNode(4);
     root->left->right = newNode(5);
 
-    vector<int> depths;
-    vector<int> widths;
-    vector<int> data;
-
-    int depth = 0;
-
-    treeDepth(root, depth, depths, data);
-
-    cout << endl << endl << endl;
-
-    int width = 0;
-    treeWidth(root, width, widths);
-
-    cout << endl << endl << endl;
-
-    widthSort(widths, depths, data);
-
+    topView(root);
 
 
 
     return 0;
-}
-
-int maxDepth(node* node)
-{
-    if (node == NULL)
-        return 0;
-    else
-    {
-        /* compute the depth of each subtree */
-        int lDepth = maxDepth(node->left);
-        int rDepth = maxDepth(node->right);
-
-        /* use the larger one */
-        if (lDepth > rDepth)
-        {
-            return(lDepth + 1);
-        }else
-        {
-            return(rDepth + 1);
-        }
-    }
 }
 
 
@@ -84,81 +67,76 @@ node* newNode(int data)
     return(Node);
 }
 
-
-
-void treeDepth(node* node, int depth, vector<int>& depths, vector<int>& data)
+void loadTopView(node* root, int width, int depth, vector<keyvalue>& treelist)
 {
-    if(node == NULL)
-    {
+    if(root == NULL)
         return;
-    }
 
-
-    depth++;
-    cout << node->data << " " << depth << endl;
-    data.push_back(node->data);
-    depths.push_back(depth);
-    treeDepth(node->left, depth, depths, data);
-    treeDepth(node->right, depth, depths, data);
-
-}
-
-
-void treeWidth(node* node, int width, vector<int>& widths)
-{
-    if(node == NULL)
+    if(treelist.size() == 0)
     {
-        return;
-    }
+        keypair.data = root->data;
+        keypair.depth = depth;
+        keypair.width = width;
+        treelist.push_back(keypair);
 
-    cout << node->data << " " << width<< endl;
-    widths.push_back(width);
-    width--;
-    treeWidth(node->left, width, widths);
-    width += 2;
-    treeWidth(node->right, width, widths);
-}
-
-void widthSort(vector<int>& widths, vector<int>& depths, vector<int>& data)
-{
-    cout << "width\tdepth\tdata\n";
-    for(int i = 0; i < data.size(); i++)
+    }else
     {
-        cout << widths.at(i) << "\t" << depths.at(i) << "\t"<< data.at(i) << endl;
-    }
-    cout << endl << endl << endl;
-    cout << endl << endl << endl;
+        bool isFound = false;
 
-    int temp = 0;
-    int mysize = data.size();
-    for(int i = 0; i < data.size() - 1; i++)
-    {
-        for (int j = i + 1; j < data.size(); j++)
+        for (int i = 0; i < treelist.size(); i++)
         {
-            if (widths.at(i) > widths.at(j))
+            if(treelist.at(i).width == width)
             {
-                temp = widths.at(i);
-                widths.at(i) = widths.at(j);
-                widths.at(j) = temp;
+                isFound = true;
 
-                temp = data.at(i);
-                data.at(i) = data.at(j);
-                data.at(j) = temp;
-
-                temp = depths.at(i);
-                depths.at(i) = depths.at(j);
-                depths.at(j) = temp;
+                if (treelist.at(i).depth > depth)
+                {
+                    treelist.at(i).width = width;
+                    treelist.at(i).depth = depth;
+                    treelist.at(i).data = root->data;
+                }
             }
         }
+
+
+        if (isFound == false)
+        {
+            if (width < 0)
+            {
+                keypair.data = root->data;
+                keypair.depth = depth;
+                keypair.width = width;
+                treelist.insert(begin(treelist), keypair);
+            }else
+            {
+                keypair.data = root->data;
+                keypair.depth = depth;
+                keypair.width = width;
+                treelist.push_back(keypair);
+            }
+        }
+
     }
-    cout << endl << endl << endl;
-    cout << endl << endl << endl;
-    cout << "width\tdepth\tdata\n";
-    for(int i = 0; i < data.size(); i++)
-    {
-        cout << widths.at(i) << "\t" << depths.at(i) << "\t"<< data.at(i) << endl;
-    }
+    depth++;
+    width -= 1;
+
+    loadTopView(root->left, width,  depth, treelist);
+    width += 2;
+    loadTopView(root->right, width,  depth, treelist);
+
+
 }
 
+void topView(node * root) {
 
+    int width = 0;
+    int depth = 0;
+    int data = 0;
+    vector<keyvalue> treelist;
+    loadTopView(root,  width, depth, treelist);
 
+    for (int i = 0; i < treelist.size(); i++)
+    {
+        cout << treelist.at(i).data << " "<<endl;
+    }
+}
